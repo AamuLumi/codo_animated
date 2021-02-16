41564f2fdcac60cf84812a05c3053b829ee5fec9
AamuLumi <florian@kauder.fr>
2021-02-16 14:59:26 +0100

    feat(app): Add animations

--- App.tsx
+++ App.tsx
@@ -1,19 +1,57 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useRef, useState } from "react";
+import {
+  Animated,
+  Button,
+  Easing,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 import SecondView from "./SecondView";
 
 export default function App() {
   const [viewOpened, setOpenView] = useState(false);
   const [backgroundColor, setBackgroundColor] = useState("#121212");
+  const secondViewAnimation = useRef(new Animated.Value(0)).current;
 
   const openSecondView = () => {
     setOpenView(true);
     setBackgroundColor("#402040");
+
+    Animated.sequence([
+      Animated.timing(secondViewAnimation, {
+        duration: 500,
+        easing: Easing.bounce,
+        toValue: 0.7,
+        useNativeDriver: false,
+      }),
+      Animated.timing(secondViewAnimation, {
+        duration: 500,
+        easing: Easing.bounce,
+        toValue: 0.3,
+        useNativeDriver: false,
+      }),
+      Animated.timing(secondViewAnimation, {
+        duration: 500,
+        easing: Easing.bounce,
+        toValue: 1,
+        useNativeDriver: false,
+      }),
+    ]).start();
   };
   const closeSecondView = () => {
-    setOpenView(false);
+    Animated.timing(secondViewAnimation, {
+      duration: 500,
+      toValue: 0,
+      useNativeDriver: false,
+    }).start();
+
+    setTimeout(() => {
+      setOpenView(false);
+    }, 500);
     setBackgroundColor("#121212");
   };
 
@@ -35,7 +73,22 @@ export default function App() {
 
         <Button onPress={openSecondView} title="Lire un livre" />
 
-        {viewOpened && <SecondView onClose={closeSecondView} />}
+        {viewOpened && (
+          <Animated.View
+            style={{
+              position: "absolute",
+              top: secondViewAnimation.interpolate({
+                inputRange: [0, 1],
+                outputRange: ["100%", "0%"],
+              }),
+              bottom: 0,
+              left: 0,
+              right: 0,
+            }}
+          >
+            <SecondView onClose={closeSecondView} />
+          </Animated.View>
+        )}
       </View>
     </SafeAreaView>
   );
